bfc0febab1e6c769193f78fb5e886f25a5872463
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2024-09-10 23:35:53 +0300

    added data

--- DragonBurn/Config/ConfigMenu.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "..\Resources\Language.h"
-
-namespace ConfigMenu {
-    //void RenderConfigMenu(const char *Tab);
-    void RenderCFGmenu();
-    void ResetToDefault();
-    // Define other configuration-related functions and variables here.
-}
