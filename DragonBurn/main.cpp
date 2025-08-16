a58a7c3b100f98945a47f1992098c1987a69aa6a
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-08-16 14:12:51 +0300

    small optimization changes

--- DragonBurn/main.cpp
+++ DragonBurn/main.cpp
@@ -1,5 +1,4 @@
-﻿//
-//______                            ______                  
+﻿//______                            ______                  
 //|  _  \                           | ___ \                 
 //| | | |_ __ __ _  __ _  ___  _ __ | |_/ /_   _ _ __ _ __  
 //| | | | '__/ _` |/ _` |/ _ \| '_ \| ___ \ | | | '__| '_ \ 
@@ -7,6 +6,8 @@
 //|___/ |_|  \__,_|\__, |\___/|_| |_\____/ \__,_|_|  |_| |_|
 //                  __/ |                                   
 //                 |___/                                    
+//
+//https://discord.gg/5WcvdzFybD
 //https://github.com/ByteCorum/DragonBurn
 
 #include "Core/Cheats.h"
@@ -57,6 +58,8 @@ void Cheat()
 |___/ |_|  \__,_|\__, |\___/|_| |_\____/ \__,_|_|  |_| |_|
                   __/ |                                   
                  |___/                                    
+
+https://discord.gg/5WcvdzFybD
 https://github.com/ByteCorum/DragonBurn
 
 
