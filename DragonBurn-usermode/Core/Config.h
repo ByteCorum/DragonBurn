54554b73143ed48772ace15a32b4d668059d7bde
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-10-23 01:48:44 +0300

    docs: new ver

--- DragonBurn-usermode/Core/Config.h
+++ DragonBurn-usermode/Core/Config.h
@@ -5,7 +5,7 @@
 namespace MenuConfig
 {
 	inline std::string name = "DragonBurn";
-	inline std::string version = "3.4.7.0";
+	inline std::string version = "3.5.7.0";
 	inline std::string author = "ByteCorum";
 
 	inline std::string path = "";
