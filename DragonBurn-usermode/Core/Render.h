b9740b04a6aac42b00c6b9a36e4e783d4764a519
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-11-13 20:02:24 +0000

    fix: hp bar

--- DragonBurn-usermode/Core/Render.h
+++ DragonBurn-usermode/Core/Render.h
@@ -352,7 +352,7 @@ namespace Render
 			ImDrawList* DrawList = ImGui::GetBackgroundDrawList();
 
 			float proportion = (MaxHealth > 0.f) ? CurrentHealth / MaxHealth : 0.f;
-			proportion = (proportion < 0.f) ? 0.f : proportion;
+			proportion = (proportion < 0.f) ? 0.f : ((proportion > 1.f) ? 1.f : proportion);
 
 			float height = RectSize.y * proportion;
 			ImVec2 rectBR = { RectPos.x + RectSize.x, RectPos.y + RectSize.y };
