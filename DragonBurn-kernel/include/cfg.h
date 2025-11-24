abb982c6982a62fd86fdfe4f28422ceb2ee1812b
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-11-23 23:08:37 -0800

    docs: new ver

--- DragonBurn-kernel/include/cfg.h
+++ DragonBurn-kernel/include/cfg.h
@@ -5,8 +5,8 @@
 
 namespace cfg
 {
-	static std::string kmVersion = xorstr_("2.4.7.1");
-	static std::string umVersion = xorstr_("3.6.9.1");
+	static std::string kmVersion = xorstr_("2.5.7.1");
+	static std::string umVersion = xorstr_("3.7.9.1");
 	static std::string name    = xorstr_("DragonBurn");
 	static std::string author  = xorstr_("ByteCorum");
 
