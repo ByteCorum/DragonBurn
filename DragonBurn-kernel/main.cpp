a1f38d56ec6e0d2c09cdb42653bbf47312aa3a17
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-11-23 21:26:31 -0800

    chore: bumped mapper web_api to last commit

--- DragonBurn-kernel/main.cpp
+++ DragonBurn-kernel/main.cpp
@@ -16,113 +16,24 @@
 
 using json = nlohmann::json;
 
-LONG WINAPI SimplestCrashHandler(EXCEPTION_POINTERS* ExceptionInfo)
-{
-	if (ExceptionInfo && ExceptionInfo->ExceptionRecord)
-	{
-		std::ostringstream oss;
-		oss << "Crash at addr 0x" << ExceptionInfo->ExceptionRecord->ExceptionAddress << L" by 0x" << std::hex << ExceptionInfo->ExceptionRecord->ExceptionCode;
-		Log::Error(oss.str(), false);
-	}
-	else
-		Log::Error("Program crashed", false);
+bool IsDriverRunning(const LPCWSTR name);
+LONG WINAPI SimplestCrashHandler(EXCEPTION_POINTERS* ExceptionInfo);
+bool CheckCheatVersion();
+bool CheckArg(const int argc, wchar_t** argv, const wchar_t* arg);
+DWORD getParentProcess();
+bool callbackExample(ULONG64* param1, ULONG64* param2, ULONG64 allocationPtr, ULONG64 allocationSize);
 
-	if (intel_driver::hDevice)
-		intel_driver::Unload();
-
-	return EXCEPTION_EXECUTE_HANDLER;
-}
-
-bool callbackExample(ULONG64* param1, ULONG64* param2, ULONG64 allocationPtr, ULONG64 allocationSize) {
-	UNREFERENCED_PARAMETER(param1);
-	UNREFERENCED_PARAMETER(param2);
-	UNREFERENCED_PARAMETER(allocationPtr);
-	UNREFERENCED_PARAMETER(allocationSize);
-	Log::Fine("Driver callback called");
-	
-	/*
-	This callback occurs before call driver entry and
-	can be useful to pass more customized params in 
-	the last step of the mapping procedure since you 
-	know now the mapping address and other things
-	*/
-	return true;
-}
-
-DWORD getParentProcess()
-{
-	HANDLE hSnapshot;
-	PROCESSENTRY32 pe32;
-	DWORD ppid = 0, pid = GetCurrentProcessId();
-
-	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	__try {
-		if (hSnapshot == INVALID_HANDLE_VALUE || hSnapshot == 0) __leave;
-
-		ZeroMemory(&pe32, sizeof(pe32));
-		pe32.dwSize = sizeof(pe32);
-		if (!Process32First(hSnapshot, &pe32)) __leave;
-
-		do {
-			if (pe32.th32ProcessID == pid) {
-				ppid = pe32.th32ParentProcessID;
-				break;
-			}
-		} while (Process32Next(hSnapshot, &pe32));
-
-	}
-	__finally {
-		if (hSnapshot != INVALID_HANDLE_VALUE && hSnapshot != 0) CloseHandle(hSnapshot);
-	}
-	return ppid;
-}
-
-bool CheckArg(const int argc, wchar_t** argv, const wchar_t* arg) 
-{
-	size_t plen = wcslen(arg);
-
-	for (int i = 1; i < argc; i++) 
-	{
-		if (wcslen(argv[i]) == plen + 1ull && _wcsicmp(&argv[i][1], arg) == 0 && argv[i][0] == '/')
-			return true;
-		else if (wcslen(argv[i]) == plen + 2ull && _wcsicmp(&argv[i][2], arg) == 0 && argv[i][0] == '-' && argv[i][1] == '-')
-			return true;
-	}
-	return false;
-}
-
-bool CheckCheatVersion()
-{
-	std::vector<std::string> versions;
-
-	std::string versionData;
-	Web::Get("https://api.jsonbin.io/v3/b/690e4759ae596e708f4b20b3", versionData);
-	json versionJson = json::parse(versionData)["record"];
-
-	if (!versionJson.contains("kernel-ver") || versionJson["kernel-ver"].is_null() || !versionJson["kernel-ver"].is_array())
-		throw std::runtime_error("Invalid json data");
-
-	for (const auto& version : versionJson["kernel-ver"])
-		versions.push_back(version.get<std::string>());
-
-	if (std::find(versions.begin(), versions.end(), cfg::kmVersion) != versions.end())
-		return true;
-	return false;
-}
-
-bool IsDriverRunning(const LPCWSTR name)
-{
-	HANDLE kernelDriver = CreateFile(name, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-    if (kernelDriver == INVALID_HANDLE_VALUE)
-        return false;
-
-	BOOL result = CloseHandle(kernelDriver);
-    return true;
-}
 
 int wmain(const int argc, wchar_t** argv)
 {
 	SetUnhandledExceptionFilter(SimplestCrashHandler);
+	
+	// Can't use --free and --indPages at the same time"
+	bool free = false; //free --> Automatically frees mapped memory after execution. Dangerous unless the driver finishes instantly
+	bool indPagesMode = CheckArg(argc, argv, L"securemode"); //indPagesMode --> Maps the driver into non-contiguous, separate memory pages. Better for stealth, but more complex
+	bool legacyImg = CheckArg(argc, argv, L"legacyimg");
+	bool copyHeader = false; //copyHeader --> Ennsures the PE headers are copied into memory	Needed for drivers that inspect their own image
+	bool passAllocationPtr = false;//passAllocationPtr --> Passes allocated memory pointer as first param to entry point. Used by custom loaders or shellcode-style drivers
 
 	Log::Custom(R"LOGO(______                            ______                  
 |  _  \                           | ___ \                 
@@ -136,17 +47,6 @@ int wmain(const int argc, wchar_t** argv)
 	Log::Info(cfg::name + " v" + cfg::umVersion + " & v" + cfg::kmVersion + " by " + cfg::author);
 	Log::Info("https://github.com/ByteCorum/DragonBurn");
 	Log::Info("https://discord.gg/5WcvdzFybD\n");
-
-	//free --> Automatically frees mapped memory after execution. Dangerous unless the driver finishes instantly
-	//indPagesMode --> Maps the driver into non-contiguous, separate memory pages. Better for stealth, but more complex
-	//copyHeader --> Ennsures the PE headers are copied into memory	Needed for drivers that inspect their own image
-	//passAllocationPtr --> Passes allocated memory pointer as first param to entry point. Used by custom loaders or shellcode-style drivers
-	// Can't use --free and --indPages at the same time"
-	bool free = false;
-	bool indPagesMode = CheckArg(argc, argv, L"securemode");
-	bool legacyImg = CheckArg(argc, argv, L"legacyimg");
-	bool copyHeader = false;
-	bool passAllocationPtr = false;
 
 	if (legacyImg)
 	{
@@ -224,4 +124,106 @@ CHECK_VER://CHECK_VER
 	Log::Fine("DragonBurn driver mapped successfully");
 	system("pause");
 	return 0;
+}
+
+LONG WINAPI SimplestCrashHandler(EXCEPTION_POINTERS* ExceptionInfo)
+{
+	if (ExceptionInfo && ExceptionInfo->ExceptionRecord)
+	{
+		std::ostringstream oss;
+		oss << "Crash at addr 0x" << ExceptionInfo->ExceptionRecord->ExceptionAddress << L" by 0x" << std::hex << ExceptionInfo->ExceptionRecord->ExceptionCode;
+		Log::Error(oss.str(), false);
+	}
+	else
+		Log::Error("Program crashed", false);
+
+	if (intel_driver::hDevice)
+		intel_driver::Unload();
+
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
+bool callbackExample(ULONG64* param1, ULONG64* param2, ULONG64 allocationPtr, ULONG64 allocationSize)
+{
+	UNREFERENCED_PARAMETER(param1);
+	UNREFERENCED_PARAMETER(param2);
+	UNREFERENCED_PARAMETER(allocationPtr);
+	UNREFERENCED_PARAMETER(allocationSize);
+	Log::Fine("Driver callback called");
+
+	/*
+	This callback occurs before call driver entry and
+	can be useful to pass more customized params in
+	the last step of the mapping procedure since you
+	know now the mapping address and other things
+	*/
+	return true;
+}
+
+DWORD getParentProcess()
+{
+	HANDLE hSnapshot;
+	PROCESSENTRY32 pe32;
+	DWORD ppid = 0, pid = GetCurrentProcessId();
+
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	__try {
+		if (hSnapshot == INVALID_HANDLE_VALUE || hSnapshot == 0) __leave;
+
+		ZeroMemory(&pe32, sizeof(pe32));
+		pe32.dwSize = sizeof(pe32);
+		if (!Process32First(hSnapshot, &pe32)) __leave;
+
+		do {
+			if (pe32.th32ProcessID == pid) {
+				ppid = pe32.th32ParentProcessID;
+				break;
+			}
+		} while (Process32Next(hSnapshot, &pe32));
+
+	}
+	__finally {
+		if (hSnapshot != INVALID_HANDLE_VALUE && hSnapshot != 0) CloseHandle(hSnapshot);
+	}
+	return ppid;
+}
+
+bool CheckArg(const int argc, wchar_t** argv, const wchar_t* arg)
+{
+	size_t plen = wcslen(arg);
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (wcslen(argv[i]) == plen + 1ull && _wcsicmp(&argv[i][1], arg) == 0 && argv[i][0] == '/')
+			return true;
+		else if (wcslen(argv[i]) == plen + 2ull && _wcsicmp(&argv[i][2], arg) == 0 && argv[i][0] == '-' && argv[i][1] == '-')
+			return true;
+	}
+	return false;
+}
+
+bool CheckCheatVersion()
+{
+	std::vector<std::string> versions;
+	json cloudVer = json::parse(Web::Get("https://api.jsonbin.io/v3/b/690e4759ae596e708f4b20b3"))["record"];
+
+	if (!cloudVer.contains("kernel-ver") || cloudVer["kernel-ver"].is_null() || !cloudVer["kernel-ver"].is_array())
+		throw std::runtime_error("Invalid json data");
+
+	for (const auto& version : cloudVer["kernel-ver"])
+		versions.push_back(version.get<std::string>());
+
+	if (std::find(versions.begin(), versions.end(), cfg::kmVersion) != versions.end())
+		return true;
+	return false;
+}
+
+bool IsDriverRunning(const LPCWSTR name)
+{
+	HANDLE kernelDriver = CreateFile(name, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+	if (kernelDriver == INVALID_HANDLE_VALUE)
+		return false;
+
+	BOOL result = CloseHandle(kernelDriver);
+	return true;
 }
