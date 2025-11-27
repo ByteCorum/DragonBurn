9cad78461d0521d7272b8b9ed59ba1863b1711b6
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-11-27 03:52:21 -0800

    docs: ver-info

--- DragonBurn-launcher/config.h
+++ DragonBurn-launcher/config.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <string>
+
+namespace config
+{
+	static const std::string author = "ByteCorum";
+	static const std::string productName = "DragonBurn";
+	static const std::string version = "1.0.0.0";
+
+
+	static const bool debug = false;
+}
